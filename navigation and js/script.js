b5f2ae6798f244086f5f0ed1430b654e49c2300a
mohamadat <31732297+mohamadat@users.noreply.github.com>
2020-11-28 16:57:32 +0100

    added flex with js

---
+++ navigation and js/script.js
@@ -0,0 +1,16 @@
+function openMenu(){
+
+  // console.log(
+  //   document.getElementsByTagName("div"));
+
+  if (document.getElementById("nav").style.display != "none") {
+    document.getElementById("nav").style.display = "none";
+  }else{
+    document.getElementById("nav").style.display = "block";
+
+  }
+// alert();
+// console.log("cdscs");
+
+}
+
