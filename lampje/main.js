4cb55b8df8435f6e7530c065dc05513c51683113
mohamadat <31732297+mohamadat@users.noreply.github.com>
2021-02-13 17:02:00 +0100

    saved

---
+++ lampje/main.js
@@ -0,0 +1,67 @@
+var button = document.getElementById('btn1');
+button.addEventListener('click', doSomthing);
+
+var image = document.getElementById('img1');
+
+const heading = document.getElementById("heading");
+
+
+
+var state = false;
+
+// let block scoop
+{
+
+
+}
+
+
+// ES6
+// var
+// let
+// const
+// arrow function
+// for of
+
+var fun1 = function(x, y){
+  return x + y;
+} 
+
+var arr = ["sam", "7am", "etc"];
+
+
+for(user of arr){
+  console.log(user);
+}
+
+for (let index = 0; index < arr.length; index++) {
+  console.log(arr[index]);
+}
+
+
+const fun2 = (x, y) => x + y;
+const fun3 = (z) => z;
+
+console.log(fun1(88,96));
+
+fun2(5,6);
+
+function doSomthing(){
+  // heading = 5;
+  heading.innerHTML = "hacked!!!";
+  
+if (state == true) {
+  img1.src  = ("/Untitled.png");
+   state = false;
+  console.log("1");
+}else{
+  img1.src  = ("/lamp2.png");
+  state = true;
+  console.log("2");
+}
+
+}
+
+
+
+
--- nyxc/album/shopping.js
+++ nyxc/album/shopping.js
@@ -8,30 +8,48 @@ if (document.readyState == 'loading') {
 
 function ready() {
 
+  var removeCartItemBtns = document.getElementsByClassName("fa-trash");
+  for (var i = 0; i<removeCartItemBtns.length; i++){
+    removeCartItemBtns[i].addEventListener('click',removeCartItem);
+  
+  }
+
+
+  var addBagBtns = document.getElementsByClassName("add-bag-btn");
+
+
+for (var index = 0; index < addBagBtns.length; index++) {
+  addBagBtns[index].addEventListener('click', addBagBtnsClicked);
+}
 
 
 }
 
 
-var addBagBtns = document.getElementsByClassName("add-bag-btn");
 
 
-for (var index = 0; index < addBagBtns.length; index++) {
-  addBagBtns[index].addEventListener('click', addBagBtnsClicked);
+
+
+
+function removeCartItem(event){
+
+  var trashClicked = event.target;
+  trashClicked.parentElement.remove();
+
   
 }
 
 
 
+
+
+
 function addBagBtnsClicked(event){
-
   var button = event.target;
-
   var title = button.parentElement.parentElement.getElementsByClassName('shop-item-title')[0].innerHTML;
   var price = button.parentElement.parentElement.getElementsByClassName('shop-item-price')[0].innerHTML;
   // var qty
-  console.log("atwa");
-
+console.log("atwa");
 console.log(title);
   addProductToCart(title , price , "1");
 
@@ -40,27 +58,70 @@ console.log(title);
 
 
 function addProductToCart(title, price, qty){
-//code
-// console.log(title);
-var cartRow = document.createElement('div');
-cartRow.classList.add('cart-row');
-cartRow.innerHTML = `<h1> ${title} , ${price} , ${qty} </h1>`;
+
+var allTitles = document.getElementsByClassName("cart-items")[0].getElementsByClassName("cart-row-title");
 
 
+// == for comparison
+// === for comparison && data type
 
-var cartItems = document.getElementsByClassName('cart-items');
+if (allTitles.length > 0) {
+  for(var i =0; i<allTitles.length ; i++){
+    if(allTitles[i].innerHTML == title){
+  // Code
+  alert("Deze item is al toegevoegd");
+  
+    }
+    else{
+      addCARTrow(title, price);
+    }
+}
+  
 
-console.log(cartItems);
+}else{
+
+  addCARTrow(title, price);
 
 
-cartItems[0].append(cartRow);
+}
+console.log(allTitles);
 
 
 
 }
 
 
-addProductToCart("dupmy1", "22" , "1");
+
+
+function addCARTrow(title, price){  
+//code
+// console.log(title);
+var cartRow = document.createElement('div');
+cartRow.classList.add('cart-row');
+cartRow.innerHTML = `
+<li>
+<span class="cart-row-title">${title}</span>
+
+ , ${price}
+<input type="number" value="1">
+<i class="fa fa-trash" aria-hidden="true"></i>
+
+
+</li>`;
+
+var cartItems = document.getElementsByClassName('cart-items');
+
+
+//1- add new Element
+cartItems[0].append(cartRow);
+
+
+//2- The add eventListner
+cartRow.getElementsByClassName("fa-trash")[0].addEventListener('click',removeCartItem);
+
+}
+
+// addProductToCart("dupmy1", "22" , "7761");
 
 
 
